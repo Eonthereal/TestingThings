e7417e8958723d412f91c3d00a0af6e6cca0694e
Aris-Michail Zografos <eon_aris@hotmail.com>
2021-03-20 16:35:23 +0200

    Aris Commit

---
+++ src/newangeliki/TestingClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newangeliki;
+
+/**
+ *
+ * @author eon_A
+ */
+public class TestingClass {
+    
+    
+    
+      public void aris(){
+       System.out.println("aris");
+   }
+}
