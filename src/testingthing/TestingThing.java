fd5f7d5a7388285717ab8c54596a21a0d1b12b47
AngelikiPan <74975215+AngelikiPan@users.noreply.github.com>
2021-03-20 16:20:01 +0200

    My first sout (Angeliki)

--- src/testingthing/TestingThing.java
+++ src/testingthing/TestingThing.java
@@ -16,6 +16,7 @@ public class TestingThing {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Angeliki's change!!!!!");
     }
     
 }
