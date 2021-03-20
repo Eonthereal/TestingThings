81d3d9fa710cd01bab3a7b92f5c7554d0b4ebf64
AngelikiPan <74975215+AngelikiPan@users.noreply.github.com>
2021-03-20 16:25:57 +0200

    Second sout (Ang)

--- src/testingthing/TestingThing.java
+++ src/testingthing/TestingThing.java
@@ -17,6 +17,7 @@ public class TestingThing {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Angeliki's change!!!!!");
+        System.out.println("Angeliki's second change!!!!!");
     }
     
 }
